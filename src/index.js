e70d10926161b24db49783fec30edfb191b4504b
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-08-24 19:50:19 +0300

    add js simplebox and css

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// Add imports above this line
+
+import { galleryItems } from './js/gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// let SimpleLightbox = require('simplelightbox');
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryListDiv = document.querySelector('.gallery');
+const cardsMarkup = createPictureCards(galleryItems);
+
+galleryListDiv.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+function createPictureCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  // captions: true,
+  captionDelay: 250,
+  // captionsData: title,
+  //   overlayOpacity: 1,
+});
